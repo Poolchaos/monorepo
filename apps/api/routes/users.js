47a2a833adeb01e3fc851fa96dd94acd20099022
Phillip-Juan van der Berg <flaapjan@zailab.com>
2020-02-09 15:13:49 +0200

    feat(payment system): fix express request system

--- apps/api/routes/users.js
+++ apps/api/routes/users.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+var fs = require('fs');
+var csv = require('csv-parser');
+
+var inputFile='mock/mock_data.csv';
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  try {
+    Promise
+      .resolve()
+      .then(() => {
+        let data = [];
+
+        fs.createReadStream(inputFile)
+          .pipe(csv())
+          .on('data', (row) => {
+            data.push(row);
+          })
+          .on('end', () => {
+            console.log('CSV file successfully processed');
+            res.send(data);
+          });
+      })
+    } catch(error) {
+      console.log(error);
+      res.sendStatus(400);
+    }
 });
 
 module.exports = router;
